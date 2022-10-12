be06c1f91a5083d773a57e0079f7cb870eed2de1
ayockson2500 <ayockson2500@gmail.com>
2022-10-12 02:47:22 +0100

    quiz qpp updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div>
       <div className='text-center mb-5'>
-        <h1>Take A Quiz</h1>
+        <h1>Take A Quiz!</h1>
       </div>
       <div className='text-center'>
       <Link to="QuestionAndAnswersDisplay" className="ui btn btn-secondary">Start</Link>
